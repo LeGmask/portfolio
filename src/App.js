0fe524495694f71382b921084b7e79909f46136e
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2020-12-02 18:15:31 +0100

    🌐 Add i18n Fixes #2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+
 import logo from "./images/logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { IntlProvider } from "react-intl";
+import translations from "./i18n/locales";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    var initLang = localStorage.getItem("locale");
+
+    if (initLang == null) initLang = this.getLang();
+    if (!(initLang in translations)) {
+      initLang = "en";
+    }
+
+    this.state = {
+      locale: initLang,
+      messages: translations[initLang],
+    };
+
+    this.onChangeLanguage = this.onChangeLanguage.bind(this);
+  }
+
+  getLang() {
+    if (navigator.languages !== undefined) return navigator.languages[0];
+    else return navigator.language;
+  }
+
+  onChangeLanguage(lang) {
+    localStorage.setItem("locale", lang);
+    this.setState({ locale: lang, messages: translations[lang] });
+  }
+
+  render() {
+    return (
+      <IntlProvider locale={this.state.locale} messages={this.state.messages}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              <FormattedMessage
+                id="Home.welcome"
+                defaultMessage="Edit <code>src/App.js</code> and save to reload."
+              />
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </IntlProvider>
+    );
+  }
 }
 
 export default App;
