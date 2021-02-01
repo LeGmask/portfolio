750ec2cf22e2ec2d688705d7716ed9594f9f9e7c
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2021-02-01 17:01:40 +0100

    🔍 Add helmet for 404

--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -1,24 +1,44 @@
 import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 
+import ReactHelmet from "../components/helmet/reactHelmet";
+import Beadcrumb from "../components/helmet/beadcrumb";
+
+import translations from "../i18n/locales";
+
 import logo from "../images/logo.svg";
 import "../App.scss";
 
 export default class NotFound extends Component {
   constructor(props) {
     super(props);
-    document.title = "404";
+
+    this.state = {
+      locale: props.locale,
+    };
   }
 
   render() {
     return (
       <div className="App">
+        <ReactHelmet
+          title={translations[this.state.locale]["notFound.title"]}
+          description={translations[this.state.locale]["notFound.description"]}
+          keywords={["notfound", "404", "non trouvé"]}
+        />
+        <Beadcrumb
+          path={window.location.pathname}
+          origin={window.location.origin}
+        />
         <header className="App-header">
+          <h1>
+            <FormattedMessage id="notFound.title" defaultMessage="404" />
+          </h1>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             <FormattedMessage
-              id="notFound.404"
-              defaultMessage="Bip, Bop, Bioup ? Vous vous êtes perdu ?"
+              id="notFound.description"
+              defaultMessage="Oh no, look like the page requested doesn't exist"
             />
           </p>
           <a
